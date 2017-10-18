35ca1abd79a0aca1be5bb715a3960820e19d811d
btrischuk <b-trischuk@hotmail.com>
2017-10-18 14:22:01 -0700

    add .eslintrc file, add lint function

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,37 @@
 var gulp = require('gulp');
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
-    browserSync = require('browser-sync');
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync'),
+    eslint = require('gulp-eslint');
     // sass        = require('gulp-sass');
 
 
-gulp.task('scripts' , function() {
+gulp.task('scripts', ['lint'] , function() {
   gulp.src('./js/*.js')
       .pipe(uglify())
       .pipe(rename({ extname: '.min.js' }))
       .pipe(gulp.dest('./build/js'))
 });
 
+gulp.task('lint', function() {
+    return gulp.src(['./js/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
 gulp.task('watch', function() {
     gulp.watch('js/*.js', ['scripts']);
- }); 
+}); 
 
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
             baseDir: "./"
         }
-    });
+});
     
     gulp.watch(['*.html', 'build/css/*.css', 'build/js/*.js']).on('change', browserSync.reload);
         
     });
-
-    gulp.task('default', ['watch', 'browser-sync']);
+    // https://www.npmjs.com/package/gulp-eslint#usage
