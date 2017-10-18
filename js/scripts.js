46a63b7b00b39b1e6ad98b0ecc159acd9cca159d
btrischuk <b-trischuk@hotmail.com>
2017-10-18 15:37:48 -0700

    add api

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
-console.log('hello');
 
+
+//  $.getJSON({
+
+var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
+url += '?' + $.param({
+  'api-key': 'dcecf5aafbd2481c8637e3375c7ffa3a'
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function (data) {
+  console.log(data);
+
+  $.each(data.results, function (i, val) {
+    console.log(val.title);
+  });
+
+}).fail(function (err) {
+  throw err;
+});
 
 
 
@@ -7,15 +26,3 @@ console.log('hello');
 // $("button").on("click", function() {
 //   $.getJSON({
 
-//   var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-//   url += '?' + $.param({
-//     'api-key': "dcecf5aafbd2481c8637e3375c7ffa3a"
-//   })''
-//   $.ajax({
-//     url: url,
-//     method: 'GET',
-//   }).done(function(result) {
-//     console.log(result);
-//   }).fail(function(err) {
-//     throw err;
-//   });
