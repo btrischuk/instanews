624efa97c4679b19491d636fb1e24073c31f07ab
btrischuk <b-trischuk@hotmail.com>
2017-10-19 14:15:22 -0700

    add if statements for multimedia

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,50 @@ var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
 url += '?' + $.param({
   'api-key': 'dcecf5aafbd2481c8637e3375c7ffa3a'
 });
+
 $.ajax({
-  url: url,
   method: 'GET',
+  url: 'https://api.nytimes.com/svc/topstories/v2/home.json' + select + '.json?api-key=dcecf5aafbd2481c8637e3375c7ffa3a'
 }).done(function (data) {
+
   console.log(data);
 
-  // var (i=0, (i>12,i++));
+
+            if (data.results.length === 0) {
+              $('.story-grid').append("<p>There appears to be no stories in this section.</p>");
+            }
+            else {
+              let home = data.results,
+              home = home.filter(function(item) {
+                return item.multimedia.length;
+              })
+
+              // }).splice(0, 12);
+    
+        //above fetches the top twelve stories
+    
+                home.forEach(function(item, index) {
+                $('.stories ul').append('<li class="home' + index +'"><div class="inner-item-wrapper"><a href="' +
+                  item.url + '"><div class="article story-' + index +'"><div class="story-meta"><p>' +
+                  item.abstract + '</p></div></div></a></div></li>');
+    
+                img = item.multimedia[4];
+                $('.story-' + index).css('background-image', 'url("' + img.url + '")');
+              });
+    
+            };
+    
+        // });
+//   var (i=0, (i>12,i++));
+//   $.each(data, function(index, value) {
+//     (.append)<ul class="repo-list"></ul>
+  
+//     $("body").append("<li>" + index + " : " + value.name + "</li>");
+//   });
+
+// for (data.title[4]){
+
+// };
 
   $.each(data.results, function ([i, val]) {
     console.log(val.title);
