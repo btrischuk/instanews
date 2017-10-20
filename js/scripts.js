81f105bc511db4332ea073db21b2476766231500
btrischuk <b-trischuk@hotmail.com>
2017-10-19 18:34:29 -0700

    add filter, splice to data input

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,73 @@
 
+$(document).ready(function() {
 
-//  $.getJSON({
+$('#selected-articles').on('change', function() {
 
-var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
-url += '?' + $.param({
-  'api-key': 'dcecf5aafbd2481c8637e3375c7ffa3a'
+  var selectedArticles = $(this).val();
+
+  $("#stories").empty();
+
+  $(#stories)
+
+  // console.log(selected-articles);
+  var url = 'https://api.nytimes.com/svc/topstories/v2/' + selectedArticles + '.json'; 
+  url += '?' + $.param({
+    'api-key': "dcecf5aafbd2481c8637e3375c7ffa3a"
 });
 
 $.ajax({
+  url:url,
   method: 'GET',
-  url: 'https://api.nytimes.com/svc/topstories/v2/home.json' + select + '.json?api-key=dcecf5aafbd2481c8637e3375c7ffa3a'
-}).done(function (data) {
+})
+.done(function (data) {
 
-  console.log(data);
+ 
+
+  $.each(data.results.filter(function(item) { return item.multimedia.length !== 0 }).splice(0, 12), function(index, value) {
+    console.log('data.results:', value)
 
 
-            if (data.results.length === 0) {
-              $('.story-grid').append("<p>There appears to be no stories in this section.</p>");
-            }
-            else {
-              let home = data.results,
-              home = home.filter(function(item) {
-                return item.multimedia.length;
-              })
+  var outputTitle = value.title;
+  var outputUrl = value.url;
+  // console.log(value)
+  var outputImage = value.multimedia[4].url;
+
+    $('#stories').append('<a href="' + url 
+
+
+    // $('#stories').append('<li>' + '<h2>' + '<a href="' + outputUrl + '">' + outputTitle + '</h2>' + outputImage + '</a>' + '</li>');
+    
+    //$('#stories').append('<li>' + outputImage + '<h2>' + outputTitle + '</h2>' + '</li>');
+});
+}); 
+});
+});
+//   console.log(data);
+
+
+//             if (data.results.length === 0) {
+//               $('.story-grid').append("<p>There appears to be no stories in this section.</p>");
+//             }
+//             else {
+//               let home = data.results,
+//               home = home.filter(function(item) {
+//                 return item.multimedia.length;
+//               })
 
               // }).splice(0, 12);
     
         //above fetches the top twelve stories
     
-                home.forEach(function(item, index) {
-                $('.stories ul').append('<li class="home' + index +'"><div class="inner-item-wrapper"><a href="' +
-                  item.url + '"><div class="article story-' + index +'"><div class="story-meta"><p>' +
-                  item.abstract + '</p></div></div></a></div></li>');
+            //     home.forEach(function(item, index) {
+            //     $('.stories ul').append('<li class="home' + index +'"><div class="inner-item-wrapper"><a href="' +
+            //       item.url + '"><div class="article story-' + index +'"><div class="story-meta"><p>' +
+            //       item.abstract + '</p></div></div></a></div></li>');
     
-                img = item.multimedia[4];
-                $('.story-' + index).css('background-image', 'url("' + img.url + '")');
-              });
+            //     img = item.multimedia[4];
+            //     $('.story-' + index).css('background-image', 'url("' + img.url + '")');
+            //   });
     
-            };
+            // };
     
         // });
 //   var (i=0, (i>12,i++));
@@ -51,12 +81,13 @@ $.ajax({
 
 // };
 
-  $.each(data.results, function ([i, val]) {
-    console.log(val.title);
+//   $.each(data.results, function ([i, val]) {
+//     console.log(val.title);
 
-  });
+//   });
 
-}).fail(function (err) {
-  throw err;
-});
+// }).fail(function (err) {
+//   throw err;
+// });
 
+// });
