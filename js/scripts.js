1a30fb72023dc0ddcdd05885d456df9f7015e6a6
btrischuk <b-trischuk@hotmail.com>
2017-10-23 18:15:11 -0700

    clean up excess code in JS

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,9 @@ $(document).ready(function() {
   var lookUp = function(){
     $('header').addClass('new-layout');
     $('.logo img').addClass('resize-logo');
-    // $('#stories').addClass('new-grid');
     var selectedArticles = $('select').val();
 
-//empty articles before loading more
+    //empty articles before loading more
 
     $('#stories').empty();
 
@@ -34,7 +33,7 @@ $(document).ready(function() {
             // filter data for articles with images and only allow 12 srticles 
 
       $.each(data.results.filter(function(item) { return item.multimedia.length !== 0 }).slice(0, 12), function(index, value) {
-        console.log('data.results:', value)
+        // console.log('data.results:', value)
 
         $('#loader').hide(); 
       
@@ -43,13 +42,12 @@ $(document).ready(function() {
         var imageQuality = value.multimedia.length -1;
         var outPutImage = value.multimedia[imageQuality].url;
         var stories = $('#stories').append('<a href="' + outPutUrl + '"><div class="news-container" style="background-image:url('+outPutImage+')"><p> ' + outPutAbstract + '</p></div></a>');
-          // console.log(stories)
 
       });
     })
     .fail(function(error){
+      $('.stories').append('reload browser');      
       throw error;
-      // $('.stories').append('reload browser');
     });
   };
 
